70c4745bcaf1d278e1a2e44bde6610360d5c4001
blessedsoy <unmigong@gmail.com>
2016-08-01 14:30:26 -0400

    created environment.rb that deletes all topics and generates new 10 topics when the app is started

---
+++ config/environment.rb
@@ -0,0 +1,16 @@
+ENV['SINATRA_ENV'] ||= "development"
+
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+
+require_all 'app'
+
+
+require_relative '../app/models/twitter_api.rb'
+Topic.delete_all
+TwitterApi.new.top_10_trends 
