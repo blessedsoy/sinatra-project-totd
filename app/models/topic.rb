71994281cdbdd4e06d1a0042a092463ed9f69c70
blessedsoy <unmigong@gmail.com>
2016-08-03 15:48:56 -0400

    removes unnecessary codes

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,14 +4,4 @@ class Topic < ActiveRecord::Base
 
   has_many :comments
 
-  # @@all = []
-
-  # def initialize (name:, url:)
-  #   @name = name
-  #   @url = url
-  #   @@all << self
-  # end
-
-
-
 end
